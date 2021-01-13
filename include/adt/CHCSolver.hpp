8166dddbf0b004d7e2db82a08cc945594f0703e9
LChernigovskaya <lidiya.chernigovskaya@gmail.com>
2021-01-13 17:32:00 +0300

    Create ADT query for all CHCs

--- include/adt/CHCSolver.hpp
+++ include/adt/CHCSolver.hpp
@@ -27,12 +27,10 @@ namespace ufo
     ExprVector assumptions;
 
     ExprVector spec;
-    ExprVector goal_assumptions;
     ExprMap replace_vars;
     std::vector<int> spec_ids;
     std::vector<size_t> adt_inds;
     ExprSet decls = ruleManager.decls;
-    Expr goal;
 
     for (auto & a : z3.getAdtConstructors()) {
       constructors.push_back(regularizeQF(a));
@@ -80,100 +78,7 @@ namespace ufo
     }
 
     for (auto & a : ruleManager.chcs) {
-      if (a.isQuery) {
-        Expr destination;
-        ExprVector cnj;
-        ExprMap matching;
-        ExprMap body_matching;
-        if (a.body->arity() > 1) {
-          for(int j = 0; j < a.body->arity(); ++j) {
-            if (isOpX<NEG>(a.body->arg(j))) {
-              destination = mkNeg(a.body->arg(j));
-            }
-            // else if (isOpX<EQ>(a.body->arg(j))) {
-            //   Expr body_elem = a.body->arg(j);
-            //   if (body_elem->left()->arity() == 1) {
-            //     body_matching[body_elem->left()] = body_elem->right();
-            //     outs() << "body matching left " << *body_elem->left() << *body_elem->right() << "\n";
-            //   }
-            //   else if (body_elem->right()->arity() == 1) {
-            //     body_matching[body_elem->right()] = body_elem->left();
-            //     outs() << "body matching right " << *body_elem->right() << *body_elem->left() << "\n";
-            //   }
-            //   cnj.push_back(a.body->arg(j));
-            // }
-            else {
-              cnj.push_back(a.body->arg(j));
-            }
-          }
-        }
-        else {
-          destination = mkNeg(a.body);
-        }
-        for (int i = 0; i < a.srcRelations.size(); i++) {
-          if (decls.find(a.srcRelations[i]) != decls.end()) {
-            size_t ind = aux[a.srcRelations[i]->left()];
-            ExprVector types;
-            ExprVector newVars;
-            types.push_back(bind::typeOf(a.srcVars[i][ind]));
-            for(int j = 0; j < a.srcRelations[i]->arity() - 2; ++j) {
-              if (j != ind) {
-                Expr e = a.srcRelations[i]->arg(j);
-                types.push_back(bind::typeOf(a.srcVars[i][j]));
-                newVars.push_back(a.srcVars[i][j]);
-              }
-            }
-            Expr rel = bind::fdecl (efac.mkTerm(a.srcRelations[i]->left()->op()), types);
-            Expr app = bind::fapp (rel, newVars);
-            matching[a.srcVars[i][ind]] = app;
-            // Expr def = mk<EQ>(a.srcVars[i][ind], app);
-            // cnj.push_back(def);
-            // replace_vars[a.srcRelations[i]->left()] = def;
-            // spec_ids.push_back(spec.size());
-            // goal_assumptions.push_back(baseDef);
-          }
-          else {
-             Expr tmp = bind::fapp (a.srcRelations[i], a.srcVars[i]);
-             cnj.push_back(tmp);
-          }
-        }
-        outs() << *mk<IMPL>(conjoin(cnj, efac), destination) << "\n";
-        outs() << *(replaceAll(mk<IMPL>(conjoin(cnj, efac), destination), matching)) << "\n";
-
-        goal = replaceAll(mk<IMPL>(conjoin(cnj, efac), destination), matching);
-        matching.clear();
-        Expr left = goal->left();
-        outs() << "left: " << *left << "\n";
-        if (isOpX<AND>(left)) {
-          for (int i = 0; i < left->arity(); ++i) {
-            if (isOpX<EQ>(left->arg(i))) {
-              if (left->arg(i)->left()->arity() == 1) {
-                matching[left->arg(i)->left()] = left->arg(i)->right();
-              }
-              else if (left->arg(i)->right()->arity() == 1) {
-                matching[left->arg(i)->right()] = left->arg(i)->left();
-              }
-            }
-          }
-        }
-        else if (isOpX<EQ>(left)) {
-          if (left->left()->arity() == 1 && !isConst(left->left())) {
-            matching[left->left()] = left->right();
-            outs() << *left->left() << " " << bind::isAdtConst(left->left()) << bind::IsVar () (left->left()) << bind::IsVar () (left->right()) << "\n";
-          }
-          else if (left->right()->arity() == 1) {
-            matching[left->right()] = left->left();
-          }
-        }
-        goal = replaceAll(goal, matching);
-        outs() << "new goal: " << *goal << "\n";
-        goal = simplifyArithm(goal);
-        goal = simplifyBool(goal);
-        if (goal->arity() > 0) {
-          goal = createQuantifiedFormula(goal, constructors);
-        }
-      }
-      else {
+      if (!a.isQuery) {
         ExprVector cnj;
         ExprMap matching;
         for (int i = 0; i < a.srcRelations.size(); i++) {
@@ -296,10 +201,223 @@ namespace ufo
     for (auto & a : assumptions) {
       outs() << *a << "\n";
     }
-    outs() << "goal:\n" << *goal << "\n";
+    for (auto & a : ruleManager.chcs) {
+      if (a.isQuery) {
+        Expr destination;
+        ExprVector cnj;
+        ExprMap matching;
+        ExprMap body_matching;
+        if (a.body->arity() > 1) {
+          for(int j = 0; j < a.body->arity(); ++j) {
+            if (isOpX<NEG>(a.body->arg(j))) {
+              destination = mkNeg(a.body->arg(j));
+            }
+            // else if (isOpX<EQ>(a.body->arg(j))) {
+            //   Expr body_elem = a.body->arg(j);
+            //   if (body_elem->left()->arity() == 1) {
+            //     body_matching[body_elem->left()] = body_elem->right();
+            //     outs() << "body matching left " << *body_elem->left() << *body_elem->right() << "\n";
+            //   }
+            //   else if (body_elem->right()->arity() == 1) {
+            //     body_matching[body_elem->right()] = body_elem->left();
+            //     outs() << "body matching right " << *body_elem->right() << *body_elem->left() << "\n";
+            //   }
+            //   cnj.push_back(a.body->arg(j));
+            // }
+            else {
+              cnj.push_back(a.body->arg(j));
+            }
+          }
+        }
+        else {
+          destination = mkNeg(a.body);
+        }
+        for (int i = 0; i < a.srcRelations.size(); i++) {
+          if (decls.find(a.srcRelations[i]) != decls.end()) {
+            size_t ind = aux[a.srcRelations[i]->left()];
+            ExprVector types;
+            ExprVector newVars;
+            types.push_back(bind::typeOf(a.srcVars[i][ind]));
+            for(int j = 0; j < a.srcRelations[i]->arity() - 2; ++j) {
+              if (j != ind) {
+                Expr e = a.srcRelations[i]->arg(j);
+                types.push_back(bind::typeOf(a.srcVars[i][j]));
+                newVars.push_back(a.srcVars[i][j]);
+              }
+            }
+            Expr rel = bind::fdecl (efac.mkTerm(a.srcRelations[i]->left()->op()), types);
+            Expr app = bind::fapp (rel, newVars);
+            matching[a.srcVars[i][ind]] = app;
+            // Expr def = mk<EQ>(a.srcVars[i][ind], app);
+            // cnj.push_back(def);
+            // replace_vars[a.srcRelations[i]->left()] = def;
+            // spec_ids.push_back(spec.size());
+            // goal_assumptions.push_back(baseDef);
+          }
+          else {
+             Expr tmp = bind::fapp (a.srcRelations[i], a.srcVars[i]);
+             cnj.push_back(tmp);
+          }
+        }
+        outs() << *mk<IMPL>(conjoin(cnj, efac), destination) << "\n";
+        outs() << *(replaceAll(mk<IMPL>(conjoin(cnj, efac), destination), matching)) << "\n";
 
-    ADTSolver sol (goal, assumptions, constructors);
-    sol.solve();
+        Expr goal = replaceAll(mk<IMPL>(conjoin(cnj, efac), destination), matching);
+        matching.clear();
+        Expr left = goal->left();
+        outs() << "left: " << *left << "\n";
+        if (isOpX<AND>(left)) {
+          for (int i = 0; i < left->arity(); ++i) {
+            if (isOpX<EQ>(left->arg(i))) {
+              if (left->arg(i)->left()->arity() == 1) {
+                matching[left->arg(i)->left()] = left->arg(i)->right();
+              }
+              else if (left->arg(i)->right()->arity() == 1) {
+                matching[left->arg(i)->right()] = left->arg(i)->left();
+              }
+            }
+          }
+        }
+        else if (isOpX<EQ>(left)) {
+          if (left->left()->arity() == 1 && !isConst(left->left())) {
+            matching[left->left()] = left->right();
+            outs() << *left->left() << " " << bind::isAdtConst(left->left()) << bind::IsVar () (left->left()) << bind::IsVar () (left->right()) << "\n";
+          }
+          else if (left->right()->arity() == 1) {
+            matching[left->right()] = left->left();
+          }
+        }
+        goal = replaceAll(goal, matching);
+        outs() << "new goal: " << *goal << "\n";
+        goal = simplifyArithm(goal);
+        goal = simplifyBool(goal);
+        if (goal->arity() > 0) {
+          goal = createQuantifiedFormula(goal, constructors);
+        }
+        outs() << "goal:\n" << *goal << "\n";
+
+        ADTSolver sol (goal, assumptions, constructors);
+        sol.solve();
+      }
+      else {
+        ExprVector cnj;
+        ExprMap matching;
+        for (int i = 0; i < a.srcRelations.size(); i++) {
+          if (decls.find(a.srcRelations[i]) != decls.end()) {
+            size_t ind = aux[a.srcRelations[i]->left()];
+            ExprVector types;
+            ExprVector newVars;
+            types.push_back(bind::typeOf(a.srcVars[i][ind]));
+            for(int j = 0; j < a.srcRelations[i]->arity() - 2; ++j) {
+              if (j != ind) {
+                Expr e = a.srcRelations[i]->arg(j);
+                types.push_back(bind::typeOf(a.srcVars[i][j]));
+                newVars.push_back(a.srcVars[i][j]);
+              }
+            }
+            Expr rel = bind::fdecl (efac.mkTerm(a.srcRelations[i]->left()->op()), types);
+            Expr app = bind::fapp (rel, newVars);
+            Expr def = mk<EQ>(app, a.srcVars[i][ind]);
+            cnj.push_back(def);
+          }
+          else {
+            Expr tmp = bind::fapp (a.srcRelations[i], a.srcVars[i]);
+            cnj.push_back(tmp);
+          }
+        }
+        outs() << "body " << *a.body << "\n";
+        if (a.body->arity() > 1) {
+          for(int j = 0; j < a.body->arity(); ++j) {
+            if (isOpX<EQ>(a.body->arg(j))) {
+              Expr body_elem = a.body->arg(j);
+
+              if (body_elem->left()->arity() == 1 
+                && std::find(a.dstVars.begin(), a.dstVars.end(), body_elem->left()) != a.dstVars.end()) {
+                outs() << "HERE LEFT\n" << *body_elem << "\n";
+                matching[body_elem->left()] = body_elem->right();
+              }
+              else if (body_elem->right()->arity() == 1 
+                && std::find(a.dstVars.begin(), a.dstVars.end(), body_elem->right()) != a.dstVars.end()) {
+                outs() << "HERE RIGHT\n" << *body_elem << "\n";
+                matching[body_elem->right()] = body_elem->left();
+              }
+              // else {
+              //   cnj.push_back(a.body->arg(j));
+              // }
+              else {
+                for (auto & v : a.dstVars) {
+                  Expr ineq = ineqSimplifier(v, body_elem);
+                  outs() << "INEQ " << *v << " " << *ineq << "\n";
+                  if (ineq->left() == v) {
+                    matching[ineq->left()] = ineq->right();
+                  }
+                }
+              }
+            }
+            else {
+              cnj.push_back(a.body->arg(j));
+            }
+          }
+        }
+        cnj.push_back(a.body);
+        Expr destination = bind::fapp (a.dstRelation, a.dstVars);
+        ExprVector vars = a.dstVars;
+        size_t ind;
+        if (decls.find(a.dstRelation) != decls.end()) {
+          ind = aux[a.dstRelation->left()];
+          ExprVector types;
+          ExprVector newVars;
+          types.push_back(bind::typeOf(a.dstVars[ind]));
+          for(int j = 0; j < a.dstRelation->arity() - 2; ++j) {
+            if (j != ind) {
+              types.push_back(bind::typeOf(a.dstVars[j]));
+              newVars.push_back(a.dstVars[j]);
+            }
+          }
+          Expr rel = bind::fdecl (efac.mkTerm(a.dstRelation->left()->op()), types);
+          Expr baseApp = bind::fapp (rel, newVars);
+          destination = mk<EQ>(baseApp, a.dstVars[ind]);
+        }
+        Expr goal = mk<IMPL>(conjoin(cnj, efac), destination);
+        outs() << "new goal: " << *goal << "\n";
+        // outs() << "First asmpt: " << *asmpt << "\n";
+        goal = replaceAll(goal, matching);
+        outs() << "After replace " << *goal << "\n";
+        // matching.clear();
+        // Expr left = asmpt->left();
+        // if (isOpX<AND>(asmpt->left())) {
+        //   for (int i = 0; i < left->arity(); ++i) {
+        //     if (isOpX<EQ>(left->arg(i))) {
+        //       if (left->arg(i)->left()->arity() == 1) {
+        //         matching[left->arg(i)->left()] = left->arg(i)->right();
+        //       }
+        //       else if (left->arg(i)->right()->arity() == 1) {
+        //         matching[left->arg(i)->right()] = left->arg(i)->left();
+        //       }
+        //     }
+        //   }
+        // }
+        // if (isOpX<EQ>(asmpt->left())) {
+        //   if (asmpt->left()->left()->arity() == 1) {
+        //     // Expr h = ineqSimplifier(asmpt->left()->left(), asmpt->left());
+        //     matching[asmpt->left()->left()] = asmpt->left()->right();
+        //     // outs() << *h << "\n";
+        //   }
+        //   else if (asmpt->left()->right()->arity() == 1) {
+        //     // Expr h = ineqSimplifier(asmpt->left()->right(), asmpt->left());
+        //     matching[asmpt->left()->right()] = asmpt->left()->left();
+        //     // outs() << *h << "\n";
+        //   }
+        // }
+        // asmpt = replaceAll(asmpt, matching);
+        // outs() << "Third asmpt " << *asmpt << "\n";
+        goal = simplifyArithm(goal);
+        goal = simplifyBool(goal);
+        // outs() << "new goal: " << *goal << "\n";
+        ADTSolver sol (goal, assumptions, constructors);
+        sol.solveNoind();
+      }
+    }
   }
 }
 
